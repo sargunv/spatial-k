6f9ec37e3df0f8e83aa0b48a658cde105f97d0d8
Sargun Vohra <sargunv@users.noreply.github.com>
2025-09-22 23:47:26 -0700

    modernize project config and dependencies

---
+++ geojson/MODULE.md
@@ -0,0 +1,4 @@
+# Module geojson
+
+An implementation of the [GeoJson standard](https://datatracker.ietf.org/doc/html/rfc7946).
+
--- turf/build.gradle.kts
+++ turf/build.gradle.kts
@@ -1,6 +1,7 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
@@ -11,102 +12,82 @@ plugins {
 
 kotlin {
     explicitApi()
+    applyDefaultHierarchyTemplate()
 
-    jvm()
-    js {
-        browser {
-        }
-        nodejs {
+    jvm {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
         }
     }
-    // For ARM, should be changed to iosArm32 or iosArm64
-    // For Linux, should be changed to e.g. linuxX64
-    // For MacOS, should be changed to e.g. macosX64
-    // For Windows, should be changed to e.g. mingwX64
 
-    linuxX64("native")
-    mingwX64("mingw")
-    macosX64("macos")
-    iosArm64()
-    iosX64()
+    js(IR) {
+        browser()
+        nodejs()
+    }
+
+    // TODO: blocked by the goncalossilva resources library used in tests
+    // wasmJs {
+    //     browser()
+    //     nodejs()
+    //     d8()
+    // }
+    //
+    // wasmWasi {
+    //     nodejs()
+    // }
+
+    // native tier 1
+    macosArm64()
     iosSimulatorArm64()
+    iosArm64()
 
-    sourceSets["commonMain"].dependencies {
-        api(project(":geojson"))
-    }
+    // native tier 2
+    macosX64()
+    iosX64()
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
 
-    sourceSets["commonTest"].dependencies {
-        implementation(kotlin("test"))
-        implementation(kotlin("test-annotations-common"))
-        implementation(libs.resources)
-    }
-
-    sourceSets["jvmMain"].dependencies {
-    }
-
-    sourceSets["jvmTest"].dependencies {
-    }
-
-    sourceSets["jsMain"].dependencies {
-    }
-
-    sourceSets["jsTest"].dependencies {
-    }
-
-    sourceSets["nativeMain"].dependencies {
-    }
-    sourceSets["nativeTest"].dependencies {
-    }
+    // native tier 3
+    mingwX64()
+    // TODO: blocked by the goncalossilva resources library used in tests
+    // androidNativeArm32()
+    // androidNativeArm64()
+    // androidNativeX86()
+    // androidNativeX64()
+    // watchosDeviceArm64()
 
     sourceSets {
-        val nativeMain by getting {}
-        getByName("macosMain").dependsOn(nativeMain)
-        getByName("mingwMain").dependsOn(nativeMain)
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(nativeMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-
-        val nativeTest by getting {}
-        getByName("macosTest").dependsOn(nativeTest)
-        getByName("mingwTest").dependsOn(nativeTest)
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(nativeTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
-
         all {
             with(languageSettings) {
                 optIn("kotlin.RequiresOptIn")
             }
+        }
+
+        commonMain.dependencies {
+            api(project(":geojson"))
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(kotlin("test-annotations-common"))
+            implementation(libs.resources)
         }
     }
 }
 
 tasks.named("jsBrowserTest") { enabled = false }
 
-tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
-    // custom output directory
-    outputDirectory.set(buildDir.resolve("$rootDir/docs/api"))
-}
-
-tasks.withType(KotlinCompile::class.java).configureEach {
-    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
-}
-
-// Working around dokka problems
-afterEvaluate {
-    tasks.named("dokkaJavadocJar").configure {
-        dependsOn(":geojson:dokkaHtml")
+dokka {
+    dokkaSourceSets {
+        configureEach {
+            includes.from("MODULE.md")
+        }
     }
 }
