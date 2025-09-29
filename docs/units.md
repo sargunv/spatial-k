1e2842df007c9b1986864ffc6eaa58a6ef5a3191
Sargun Vohra <sargunv@users.noreply.github.com>
2025-09-28 21:19:06 -0700

    expand units docs (#187)

--- docs/units.md
+++ docs/units.md
@@ -1,7 +1,8 @@
 # Units
 
 The `units` module contains utilities for working with units of measure, like
-length and area.
+[Length](api/units/org.maplibre.spatialk.units/-length/index.html) and
+[Area](api/units/org.maplibre.spatialk.units/-area/index.html).
 
 Details can be found in the [API reference](./api/units/index.html).
 
@@ -26,10 +27,36 @@ Details can be found in the [API reference](./api/units/index.html).
     }
     ````
 
-## Example
+## Simple unit conversion
+
+`Length` and `Area` measurements are type safe wrappers around `Double` values,
+and can be converted to/from numbers with unit conversion.
 
 === "Kotlin"
 
     ```kotlin
-    --8<-- "units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt:example"
+    --8<-- "units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt:conversion"
+    ```
+
+## Arithmetic
+
+Measurements support common arithmetic operations, and will convert between
+scalars, lengths, and areas as needed.
+
+=== "Kotlin"
+
+    ```kotlin
+    --8<-- "units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt:arithmetic"
+    ```
+
+## Custom units
+
+We provide common international units already defined, but if you need to work
+with other units, you can define your own.
+
+=== "Kotlin"
+
+    ```kotlin
+    --8<-- "units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt:customUnits1"
+    --8<-- "units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt:customUnits2"
     ```
--- units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt
+++ units/src/commonTest/kotlin/org/maplibre/spatialk/units/DocSnippets.kt
@@ -2,6 +2,7 @@
 
 package org.maplibre.spatialk.units
 
+import kotlin.math.PI
 import kotlin.test.Test
 
 // These snippets are primarily intended to be included in docs/turf.md. Though they exist as
@@ -9,13 +10,41 @@ import kotlin.test.Test
 
 class DocSnippets {
     @Test
-    fun example() {
-        // --8<-- [start:example]
+    fun conversion() {
+        // --8<-- [start:conversion]
         val distance: Length = 123.miles
         println(distance.inKilometers)
 
         val area: Area = 45.acres
         println(area.inSquareMeters)
-        // --8<-- [end:example]
+        // --8<-- [end:conversion]
+    }
+
+    @Test
+    fun arithmetic() {
+        // --8<-- [start:arithmetic]
+        val manhattanBlock: Area = (1.miles / 20) * (1.miles / 7)
+        val chicagoBlock: Area = 330.feet * 660.feet
+        val ratio: Double = manhattanBlock / chicagoBlock
+        // --8<-- [end:arithmetic]
+    }
+
+    // --8<-- [start:customUnits1]
+    data object AmericanFootballField : AreaUnit {
+        override val metersSquaredPerUnit: Double = 109.728 * 48.8
+        override val symbol: String = "football fields"
+    }
+
+    // --8<-- [end:customUnits1]
+
+    @Test
+    fun customUnits() {
+        // --8<-- [start:customUnits2]
+        // how many football fields could fit on the earth's oceans?
+        val earthRadius: Length = 6371.kilometers
+        val earthSurface: Area = 4 * PI * earthRadius * earthRadius
+        val oceanSurface: Area = 0.7 * earthSurface
+        oceanSurface.roundToLong(AmericanFootballField)
+        // --8<-- [end:customUnits2]
     }
 }
